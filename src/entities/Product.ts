302f7430053663cf2b02e02056bec4f006e4b4f6
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-09 20:22:01 +0500

    Add src/entities/Product.ts

---
+++ src/entities/Product.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name!: string;
+
+  @Column({ type: 'text' })
+  description!: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price!: number;
+
+  @Column({ type: 'varchar', length: 500, nullable: true })
+  imageUrl!: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  category!: string;
+
+  @Column({ type: 'int', default: 0 })
+  stock!: number;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
